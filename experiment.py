6feff06fdcc3a53794617801f4fd19be6a109481
Stanny <stannygoffin@live.nl>
2025-09-27 13:10:23 +0200

    Refactoring config

--- experiment.py
+++ experiment.py
@@ -31,9 +31,10 @@ class Experiment():
         # p05 = Player(experiment, name='learningRate=0.001;preselect', learningRate=0.001, useProbabilities=True, preselectBatch=True)
         # p06 = Player(experiment, name='learningRate=0.0001;preselect', learningRate=0.0001, useProbabilities=True, preselectBatch=True)
         # p07 = Player(experiment, name='learningRate=0.00001;preselect', learningRate=0.00001, useProbabilities=True, preselectBatch=True)
-        p08 = Player(experiment, name='learningRate=0.00001;preselect;10epochs', learningRate=0.00001, useProbabilities=True, preselectBatch=True)
+        # p08 = Player(experiment, name='learningRate=0.00001;preselect;10epochs', learningRate=0.00001, useProbabilities=True, preselectBatch=True)
+        p09 = Player(experiment, name='learningRate=0.00001;10epochs', learningRate=0.00001, useProbabilities=True)
 
-        self._players = [p08]#[p00, p01, p02, p03, p04, p05, p06, p07]
+        self._players = [p09]#[p00, p01, p02, p03, p04, p05, p06, p07]
 
         # Initialize results paths
         experiment_path = os.path.join(os.getcwd(), experiment)
--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import subprocess
 
 # Run the experiment part-by-part to prevent intermittant memory issues
 while True:
-    result = subprocess.run(["python", "main.py", "experiment-20250925-1840"])
+    result = subprocess.run(["python", "main.py", "experiment-20250925-1903"])
     if result.returncode != 0:
         break
